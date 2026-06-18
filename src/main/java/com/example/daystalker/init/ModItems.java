c2793bc398f334f3259439fbb57c442046cfa1e8
martin <martin.ivansson@gmail.com>
2026-06-18 22:14:25 +0300

    Chicken sword

--- src/main/java/com/example/daystalker/init/ModItems.java
+++ src/main/java/com/example/daystalker/init/ModItems.java
@@ -2,6 +2,7 @@ package com.example.daystalker.init;
 
 import com.example.daystalker.DaystalkerMod;
 import com.example.daystalker.item.LavaSwordItem;
+import com.example.daystalker.item.ChickenSwordItem;
 import com.example.daystalker.item.LavaTier;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,6 +18,15 @@ public class ModItems {
     public static final RegistryObject<LavaSwordItem> LAVA_SWORD =
             ITEMS.register("lava_sword",
                     () -> new LavaSwordItem(
+                            LavaTier.INSTANCE,
+                            3,      // +3 attack damage on top of tier bonus = 7 total (like diamond sword)
+                            -2.4F,  // Standard sword attack speed
+                            new Item.Properties()
+                    ));
+
+    public static final RegistryObject<ChickenSwordItem> CHICKEN_SWORD =
+            ITEMS.register("chicken_sword",
+                    () -> new ChickenSwordItem(
                             LavaTier.INSTANCE,
                             3,      // +3 attack damage on top of tier bonus = 7 total (like diamond sword)
                             -2.4F,  // Standard sword attack speed
